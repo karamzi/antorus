ed46d9ab9a63c940bfc813b9d760e88f07cefa26
Maxim <maxim.panasencko@gmail.com>
2021-02-25 17:23:32 +0300

    add oprions

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Products)
+
+class RequiredOptionAdmin(admin.TabularInline):
+    model = models.RequiredOption
+    extra = 1
+
+
+class AdditionsOptionAdmin(admin.TabularInline):
+    model = models.AdditionOptions
+    extra = 1
+
+
+class ProductsAdmin(admin.ModelAdmin):
+    inlines = [RequiredOptionAdmin, AdditionsOptionAdmin]
+
+
+admin.site.register(models.Products, ProductsAdmin)
 admin.site.register(models.Categories)
 admin.site.register(models.SubCategories)
--- main/models.py
+++ main/models.py
@@ -33,11 +33,18 @@ class Categories(models.Model):
 class SubCategories(models.Model):
     category = models.ForeignKey(Categories, related_name='categories_subcategories', verbose_name='Категория',
                                  on_delete=models.PROTECT)
-    name = models.CharField(max_length=255, verbose_name='Название подкатегории')
+    name = models.CharField(max_length=255, verbose_name='Название подкатегории', unique=True)
     slug = models.SlugField(verbose_name='Слаг')
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        slug = self.name.strip()
+        slug = slug.lower()
+        slug = slug.replace(' ', '-')
+        self.slug = slug
+        super().save(*args, **kwargs)
 
     def get_absolute_url(self):
         return reverse('subcategory', kwargs={'category': self.category.slug, 'subcategory': self.slug})
@@ -50,9 +57,9 @@ class SubCategories(models.Model):
 class Products(models.Model):
     category = models.ForeignKey(Categories, related_name='products_category', verbose_name='Категория',
                                  on_delete=models.PROTECT)
-    subcategory = models.ForeignKey(SubCategories, related_name='products_subcategory', blank=True,
+    subcategory = models.ForeignKey(SubCategories, related_name='products_subcategory', blank=True, null=True,
                                     verbose_name='Подкатегория', on_delete=models.PROTECT)
-    name = models.CharField(max_length=255, verbose_name='Название товара')
+    name = models.CharField(max_length=255, verbose_name='Название товара', unique=True)
     description = models.TextField(verbose_name='Описание')
     image = models.ImageField(verbose_name='Изображение', upload_to=get_img_path)
     slug = models.SlugField(verbose_name='Слаг')
@@ -64,9 +71,52 @@ class Products(models.Model):
     def __str__(self):
         return self.name
 
+    def save(self, *args, **kwargs):
+        slug = self.name.strip()
+        slug = slug.lower()
+        slug = slug.replace(' ', '-')
+        self.slug = slug
+        super().save(*args, **kwargs)
+
     def get_absolute_url(self):
         return reverse('product', kwargs={'slug': self.slug})
 
     class Meta:
         verbose_name = 'Товар'
         verbose_name_plural = 'Товары'
+
+
+class RequiredOption(models.Model):
+    product = models.ForeignKey(Products, related_name='product_required_option', on_delete=models.PROTECT,
+                                verbose_name='Товар')
+    parent_option = models.ForeignKey('self', related_name='child_options', on_delete=models.PROTECT,
+                                      verbose_name='Родительская опция', blank=True, null=True)
+    name = models.CharField(max_length=255, verbose_name='Название опции')
+    price_dollar = models.IntegerField(default=0, verbose_name='Цена в долларах')
+    price_euro = models.IntegerField(default=0, verbose_name='Цена в евро')
+    new_price_dollar = models.IntegerField(verbose_name='Новая цена в долларах', blank=True, null=True)
+    new_price_euro = models.IntegerField(verbose_name='Новая цена в евро', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Обязательная опция'
+        verbose_name_plural = 'Обязательные опции'
+
+
+class AdditionOptions(models.Model):
+    product = models.ForeignKey(Products, related_name='product_addition_option', on_delete=models.PROTECT,
+                                verbose_name='Товар')
+    name = models.CharField(max_length=255, verbose_name='Название опции')
+    price_dollar = models.IntegerField(default=0, verbose_name='Цена в долларах')
+    price_euro = models.IntegerField(default=0, verbose_name='Цена в евро')
+    new_price_dollar = models.IntegerField(verbose_name='Новая цена в долларах', blank=True, null=True)
+    new_price_euro = models.IntegerField(verbose_name='Новая цена в евро', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Дополнительная опция'
+        verbose_name_plural = 'Дополнительные опции'
