3f6057112427465aa86cddec20a817a8d8db6812
Maxim <maxim.panasencko@gmail.com>
2021-02-25 14:33:55 +0300

    add prodcut, category, cart pages

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('product/<slug:slug>/', views.product, name='product'),
+    path('category/<slug:slug>/', views.category, name='category'),
+    path('cart/', views.cart, name='cart'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def product(request, slug):
+    return render(request, 'product.html')
+
+
+def category(request, slug):
+    return render(request, 'category.html')
+
+
+def cart(request):
+    return render(request, 'cart.html')
