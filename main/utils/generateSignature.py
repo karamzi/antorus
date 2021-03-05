9fbddb2e53b0f6d1ad560a5631efb8630bc7a1b3
Maxim <maxim.panasencko@gmail.com>
2021-03-05 13:07:34 +0300

    admin transactions

--- main/utils/generateSignature.py
+++ main/utils/generateSignature.py
@@ -2,7 +2,7 @@ import hashlib
 
 
 def generate_signature(amount, currency, order_desc, order_id):
-    sting = 'ayvZEvLCS0lAjDfD8LLwry4s282wOWmr|'+amount+'|'+currency+'|1470496|'+order_desc+'|'+order_id+'|http://151.248.114.152/|http://151.248.114.152/fondyCallback/'
+    sting = 'ayvZEvLCS0lAjDfD8LLwry4s282wOWmr|'+amount+'|'+currency+'|600|1470496|'+order_desc+'|'+order_id+'|http://151.248.114.152/|http://151.248.114.152/fondyCallback/'
     signature = hashlib.sha1(sting.encode())
     signature = signature.hexdigest()
     return signature
