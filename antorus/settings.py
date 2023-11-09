bdc749d1f7c67bbc72a9a79d8b5ee554ccf81a85
Maxim <maxim.panasencko@gmail.com>
2023-11-09 16:41:49 +0300

    правки стилей, новый баннер

--- antorus/settings.py
+++ antorus/settings.py
@@ -140,7 +140,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / "static",
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
